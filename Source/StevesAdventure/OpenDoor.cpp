5ca19cfa9e4cb5bc31af5658300d0604fb881082
Rex-22 <rex22@live.co.za>
2018-06-24 19:16:30 +0200

    Final Commit

--- Source/StevesAdventure/OpenDoor.cpp
+++ Source/StevesAdventure/OpenDoor.cpp
@@ -8,48 +8,33 @@ UOpenDoor::UOpenDoor()
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-
+	if (!pressurePlate) { UE_LOG(LogTemp, Error, TEXT("No Presure Plate set")) }
 	owner = GetOwner();
 }
 
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-	
-	if (GetTotalMassOfActorOnPlates() > triggerMass) 
-	{
-		OpenDoor();
-		lastDoorOpenTime = GetWorld()->GetTimeSeconds();
-	}
 
-	if ((GetWorld()->GetTimeSeconds() - lastDoorOpenTime) > doorCloseDelay) 
+	if (GetTotalMassOfActorOnPlates() > triggerMass)
 	{
-		CloseDoor();
+		onOpenRequest.Broadcast();
+	}
+	else {
+		onCloseRequest.Broadcast();
 	}
 }
 
-void UOpenDoor::OpenDoor()
-{
-	owner->SetActorRotation(FRotator(0.0f, openAngle, 0.0f));
-}
-
-void UOpenDoor::CloseDoor()
-{
-	owner->SetActorRotation(FRotator(0.0f, 0.0f, 0.0f));
-}
-
-float UOpenDoor::GetTotalMassOfActorOnPlates() 
-{
+float UOpenDoor::GetTotalMassOfActorOnPlates() {
 	float totalMass = 0.0f;
 
 	TArray<AActor*> overlappingActors;
+	if (!pressurePlate) { return 0.0f; }
 	pressurePlate->GetOverlappingActors(overlappingActors);
 
-	for (const auto* actor : overlappingActors)
-	{
+	for (const auto* actor : overlappingActors) {
 		totalMass += actor->FindComponentByClass<UPrimitiveComponent>()->GetMass();
 	}
 
 	return totalMass;
 }
-
